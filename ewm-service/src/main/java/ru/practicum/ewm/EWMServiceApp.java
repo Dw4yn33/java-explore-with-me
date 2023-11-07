41fa3c3f2550212e444da2c00dd3be90c22d1f66
Dw4yn33 <vova-kalinich@mail.ru>
2023-11-07 20:59:26 +0300

    Дипломный проект. Основной сервис.

--- ewm-service/src/main/java/ru/practicum/ewm/EWMServiceApp.java
+++ ewm-service/src/main/java/ru/practicum/ewm/EWMServiceApp.java
@@ -2,8 +2,27 @@ package ru.practicum.ewm;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
+import ru.practicum.ewm.category.controller.CategoryAdminController;
+import ru.practicum.ewm.category.controller.CategoryPublicController;
+import ru.practicum.ewm.compilation.controller.CompilationAdminController;
+import ru.practicum.ewm.compilation.controller.CompilationPublicController;
+import ru.practicum.ewm.event.controller.EventAdminController;
+import ru.practicum.ewm.event.controller.EventPrivateController;
+import ru.practicum.ewm.event.controller.EventPublicController;
+import ru.practicum.ewm.request.controller.RequestPrivateController;
+import ru.practicum.ewm.user.controller.UserAdminController;
 
 @SpringBootApplication
+@ComponentScan(basePackageClasses = CategoryAdminController.class)
+@ComponentScan(basePackageClasses = CategoryPublicController.class)
+@ComponentScan(basePackageClasses = CompilationAdminController.class)
+@ComponentScan(basePackageClasses = CompilationPublicController.class)
+@ComponentScan(basePackageClasses = EventAdminController.class)
+@ComponentScan(basePackageClasses = EventPrivateController.class)
+@ComponentScan(basePackageClasses = EventPublicController.class)
+@ComponentScan(basePackageClasses = RequestPrivateController.class)
+@ComponentScan(basePackageClasses = UserAdminController.class)
 public class EWMServiceApp {
 
     public static void main(String[] args) {
