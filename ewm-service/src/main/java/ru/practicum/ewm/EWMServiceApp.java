0bd436c5be7291a1093ef462e39053c09953564d
Dw4yn33 <vova-kalinich@mail.ru>
2023-11-08 19:42:12 +0300

    Дипломный проект. Комментарии. Попытка №2.

--- ewm-service/src/main/java/ru/practicum/ewm/EWMServiceApp.java
+++ ewm-service/src/main/java/ru/practicum/ewm/EWMServiceApp.java
@@ -2,16 +2,6 @@ package ru.practicum.ewm;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.ComponentScan;
-import ru.practicum.ewm.category.controller.CategoryAdminController;
-import ru.practicum.ewm.category.controller.CategoryPublicController;
-import ru.practicum.ewm.compilation.controller.CompilationAdminController;
-import ru.practicum.ewm.compilation.controller.CompilationPublicController;
-import ru.practicum.ewm.event.controller.EventAdminController;
-import ru.practicum.ewm.event.controller.EventPrivateController;
-import ru.practicum.ewm.event.controller.EventPublicController;
-import ru.practicum.ewm.request.controller.RequestPrivateController;
-import ru.practicum.ewm.user.controller.UserAdminController;
 
 @SpringBootApplication
 public class EWMServiceApp {
